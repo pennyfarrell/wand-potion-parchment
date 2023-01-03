3de36628209031095dbf6bedc3833e9c8bb167ed
pennyfarrell <penusia.farrell@gmail.com>
2023-01-03 19:58:19 +0000

    Add JS gamePlay function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 function playGame(playerChoice) {
-
+const computerChoice = Math.random() < 0.5 ? "wand" : "potion";
+if (computerChoice === "wand") {
+    (computerChoice === "parchment";
+}
+else {
+    computerChoice = "wand";
+}
+if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+}
+else if (playerChoice === "wand" && computerChoice === "potion")
+{ result = "You win!";
+}
 }
