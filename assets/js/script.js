9dd70b685c2807efe03bf29827221e3bc5eaf98c
pennyfarrell <penusia.farrell@gmail.com>
2023-01-04 22:07:58 +0000

    Fix error in JS for result display

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,40 @@ window.onload = function() {
     document.getElementById("computer-score").innerHTML = 0;
   };
   
-function playGame(playerChoice) {
-    //Determine computer choice randomly
+  function playGame(playerChoice) {
+    // Determine computer choice randomly
     const computerChoice = Math.random() < 0.5 ? "wand" : "potion";
-    // if (computerChoice === "wand") {
-    //     computerChoice = "parchment";
-    // } else if (computerChoice === "potion") {
-    //     computerChoice = "wand";
-    // }
-    
+  
     let result;
-    if (playerChoice === computerChoice) {
-        result = "It's a tie!";
-    } else if (playerChoice === "wand" && computerChoice === "potion") {
-        result = "You win!";
-
-        document.getElementById("your-score").innerHTML = parseInt(document.getElementById("your-score").innerHTML) + 1; 
+    if (playerChoice === "wand" && computerChoice === "potion") {
+      result = "You win!";
+      document.getElementById("your-score").innerHTML = parseInt(document.getElementById("your-score").innerHTML) + 1;
+    } else if (playerChoice === "potion" && computerChoice === "wand") {
+      result = "You lose!";
+      document.getElementById("computer-score").innerHTML = parseInt(document.getElementById("computer-score").innerHTML) + 1;
+    } else if (playerChoice === "parchment" && computerChoice === "potion") {
+      result = "You lose!";
+      document.getElementById("computer-score").innerHTML = parseInt(document.getElementById("computer-score").innerHTML) + 1;
     } else if (playerChoice === "potion" && computerChoice === "parchment") {
-                result = "You win!";
-                document.getElementById("your-score").innerHTML = parseInt(document.getElementById("your-score").innerHTML) + 1;
-     } else if (playerChoice === "parchment" && computerChoice === "wand") {
-            result = "You win!";
-            document.getElementById("your-score").innerHTML = parseInt(document.getElementById("your-score").innerHTML) + 1;
-      } else {
-            result = "You lose!";
-    
-document.getElementById("computer-score").innerHTML = parseInt(document.getElementById("computer-score").innerHTML) + 1;
-        }     
+      result = "You win!";
+      document.getElementById("your-score").innerHTML = parseInt(document.getElementById("your-score").innerHTML) + 1;
+    } else if (playerChoice === "wand" && computerChoice === "parchment") {
+      result = "You lose!";
+      document.getElementById("computer-score").innerHTML = parseInt(document.getElementById("computer-score").innerHTML) + 1;
+    } else if (playerChoice === "parchment" && computerChoice === "wand") {
+      result = "You win!"
+  
 
         // Update results on the page
-document.querySelector(".results").innerHTML = `You chose ${playerChoice.textContent}. <br> The wizard chose ${computerChoice}. <br> ${result}`;
+        document.querySelector(".results").innerHTML = `You chose ${playerChoice.innerHTML.trim()}. <br> The wizard chose ${computerChoice}. <br> ${result}`;
     }
 
     //Event listener for button click
-    document.getElementById("wand-button").addEventListener("click", () => playGame(event.target));
-    document.getElementById("potion-button").addEventListener("click", () => playGame(event.target));
-    document.getElementById("parchment-button").addEventListener("click", () => playGame(event.target));
+    document.getElementById("wand-button").addEventListener("click", () => playGame("wand"));
+document.getElementById("potion-button").addEventListener("click", () => playGame("potion"));
+document.getElementById("parchment-button").addEventListener("click", () => playGame("parchment"));
+
+    
 
     //Add event listener for reset button
     document.querySelector(".reset button").addEventListener("click", () => {
